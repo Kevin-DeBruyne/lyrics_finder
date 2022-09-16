0d5c511b8a74655a6a7bee974517b193f62b0df6
Kevin-DeBruyne <mukulupadhyay073@gmail.com>
2022-09-16 12:02:43 +0000

    Update 3 files

---
+++ src/Navbar.js
@@ -0,0 +1,22 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function ColorSchemesExample() {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">LyricsFinder</Navbar.Brand>
+          
+        </Container>
+      </Navbar>
+      <br />
+      
+
+     
+    </>
+  );
+}
+
+export default ColorSchemesExample;
